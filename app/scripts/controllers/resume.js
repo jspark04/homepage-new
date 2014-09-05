5450cbddc4a01072a66a831b544a20b53fc0e55d
John Park <johnpark.rice@gmail.com>
2014-09-05 05:11:34 -0500

    An update.

---
+++ app/scripts/controllers/resume.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name homepageNewApp.controller:ResumeCtrl
+ * @description
+ * # ResumeCtrl
+ * Controller of the homepageNewApp
+ */
+angular.module('homepageNewApp')
+  .controller('ResumeCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
